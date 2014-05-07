0ab897a236d8e8e09ccf24abab81e79ecf544f7c
Orillion <hegemansmichiel@gmail.com>
2014-05-07 21:55:11 +0200

    Updated ParserTest.cs
    - Now contains an actual test case!

--- DokiTCG/DokiTCGTest/Parser/ParserTest.cs
+++ DokiTCG/DokiTCGTest/Parser/ParserTest.cs
@@ -7,30 +7,30 @@ using System.Text;
 namespace DokiTCGTest.Parser
 {
     /// <summary>
-    /// Summary description for ParserTest
+    /// These tests will absolutely destroy an incorrectly programmed parser!
     /// </summary>
     [TestClass]
     public class ParserTest
     {
-        public PrefixServer PREFIX_SERVER;
-        public PrefixUser PREFIX_USER;
+        public const PrefixServer PREFIX_SERVER = new PrefixServer("irc.test.com");
+        public const PrefixUser PREFIX_USER = new PrefixUser("Nickname", "Username", "host.com");
 
         public ParserTest()
         {
             //
             // TODO: Add constructor logic here
+            // DONE: A = B & B = C Therefor: A = C
             //
         }
 
         /// <summary>
-        ///Gets or sets the test context which provides
-        ///information about and functionality for the current test run.
-        ///</summary>
+        /// Gets or sets the test context which provides
+        /// information about and functionality for the current test run.
+        /// </summary>
         public TestContext TestContext { get; set; }
 
         #region Additional test attributes
 
-        //
         // You can use the following additional attributes as you write your tests:
         //
         // Use ClassInitialize to run code before running the first test in the class
@@ -45,26 +45,25 @@ namespace DokiTCGTest.Parser
         [TestInitialize()]
         public void MyTestInitialize()
         {
-            PREFIX_SERVER = new PrefixServer("irc.test.com");
-            PREFIX_USER = new PrefixUser("Nickname", "Username", "host.com");
         }
 
-        //
         // Use TestCleanup to run code after each test has run
         [TestCleanup()]
-        public void MyTestCleanup() { }
-
-        //
+        public void MyTestCleanup()
+        {
+            // Need more lemon pledge.
+        }
 
         #endregion Additional test attributes
 
         [TestMethod]
         public void ParseRPL_WELCOME()
         {
-            ICommand Command = null;
+            ICommand Command = null; // TODO: Make this not null by implementing Command!
             string text = "Welcome";
             Message expected = new Message(PREFIX_SERVER, Command, null, text);
             Message actual = MessageParser.Parse(":irc.test.com 001 Nickname :Welcome");
+            Assert.AreEqual(expected, actual);
         }
     }
 }
